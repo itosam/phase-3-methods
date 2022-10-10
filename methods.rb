3df7e2f7d3b02e80e37fb328de76e432c2ac124d
Osamu Ito <mr.samito@gmail.com>
2022-10-10 12:23:58 -0400

    commit tree 55799518bfdc7baf670adca8a1bf39d9f7135bf7

--- methods.rb
+++ methods.rb
@@ -1,1 +1,21 @@
-# Your code here!
+# methods in ruby are like functions in javascript
+def greet_programmer
+    puts"Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    return num1+num2
+end
+
+def halve(num)
+    return nil unless num.class == Integer
+    num / 2
+end
